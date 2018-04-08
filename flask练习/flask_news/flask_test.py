ac7b0a45a2a8c043421c65e6a3216dee6864fe16
qinl <dreamkong0113@gmail.com>
2018-04-08 15:08:55 +0800

    使用flask+mongodb 实现的简易版新闻移动端

--- flask练习/flask_news/flask_test.py
+++ flask练习/flask_news/flask_test.py
@@ -1,45 +1,85 @@
 from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_mongoengine import MongoEngine
 from datetime import datetime
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:3306/wangyi_news?charset=utf8'
-db = SQLAlchemy(app)
+# mysql
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:3306/wangyi_news?charset=utf8'
+# mongodb
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'mongo_news',
+    'host': 'localhost',
+    'port': 27017
+}
+# SQLAlchemy
+# db = SQLAlchemy(app)
+# MongoEngine
+db = MongoEngine(app)
 
 
-class News(db.Model):
-    __tablename__ = 'news'
+# class News(db.Model):
+#     __tablename__ = 'news'
 
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
-    content = db.Column(db.String(2000), nullable=False)
-    types = db.Column(db.String(10), nullable=False)
-    image = db.Column(db.String(300))
-    author = db.Column(db.String(20))
-    view_count = db.Column(db.Integer)
-    created_at = db.Column(db.DATETIME)
-    is_valid = db.Column(db.Boolean)
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(200), nullable=False)
+#     content = db.Column(db.String(2000), nullable=False)
+#     types = db.Column(db.String(10), nullable=False)
+#     image = db.Column(db.String(300))
+#     author = db.Column(db.String(20))
+#     view_count = db.Column(db.Integer)
+#     created_at = db.Column(db.DATETIME)
+#     is_valid = db.Column(db.Boolean)
 
-    def __repr__(self):
-        return '<News %r> ' % self.title
+#     def __repr__(self):
+#         return '<News %r> ' % self.title
+
+class News(db.Document):
+    title = db.StringField(max_length=200, required=True)
+    content = db.StringField(required=True)
+    types = db.StringField(requiresd=True)
+    image = db.StringField()
+    is_valid = db.BooleanField(default=True)
+    created_at = db.DateTimeField(default=datetime.now())
+    updated_at = db.DateTimeField(default=datetime.now())
+
+    meta = {
+        'collection': 'news',
+        'ordering': ['-created_at']
+    }
+
+
+def add_data():
+    new_obj = News(
+        title='惊现UFO！',
+        content='我是新闻内容',
+        types='推荐',
+        image='http://www.chinanews.com/yl/yl-ypkb/news/2009/07-31/U149P4T8D1799129F107DT20090731133550.jpg',
+    )
+    new_obj.save()
+    return new_obj
 
 
 @app.route('/')
 def index():
-    news_list = News.query.all()
+    # news_list = News.query.all()
+    news_list = News.objects.all()
     return render_template('index.html', news_list=news_list)
 
 
 @app.route('/cat/<name>/')
 def cat(name):
-    news_list = News.query.filter(News.types == name)
+    # news_list = News.query.filter(News.types == name)
+    news_list = News.objects.filter(types=name)
     return render_template('cat.html', name=name, news_list=news_list)
 
 
-@app.route('/detail/<int:pk>/')
+@app.route('/detail/<pk>/')
 def detail(pk):
-    news = News.query.get(pk)
+    # news = News.query.get(pk)
+    news = News.objects.filter(pk=pk).first_or_404()
     return render_template('detail.html', news=news)
 
 
