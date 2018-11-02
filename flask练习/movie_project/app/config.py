e49c13d10dcf3bcab12c20bdfdbecf03d6056d9f
qinl <dreamkong0113@gmail.com>
2018-11-02 17:02:49 +0800

    flask构建微电影网站标签列表、添加、修改逻辑

--- flask练习/movie_project/app/config.py
+++ flask练习/movie_project/app/config.py
@@ -1,2 +1,10 @@
+import os
+
 SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:root@localhost:3306/movie'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+SECRET_KEY = 'LLa7OxjE0XgTIOYxv1IgxAQ3y9rlmn'
+UP_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/uploads/')
+
+print(os.path.dirname(__file__))
+print(os.path.abspath(os.path.dirname(__file__)))
+print(UP_DIR)
