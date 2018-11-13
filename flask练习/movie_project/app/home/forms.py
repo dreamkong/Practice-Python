3d55a66fddebd862c5d7350477f3e96aa30f84de
qinl <dreamkong0113@gmail.com>
2018-11-13 14:50:07 +0800

    flask构建微电影网站前台用户信息修改逻辑

--- flask练习/movie_project/app/home/forms.py
+++ flask练习/movie_project/app/home/forms.py
@@ -3,7 +3,7 @@
 Created by dreamkong on 2018/10/31
 """
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, FileField, TextAreaField
 
 from wtforms.validators import DataRequired, EqualTo, Email, Regexp, ValidationError
 
@@ -131,5 +131,75 @@ class LoginForm(FlaskForm):
         label='登录',
         render_kw={
             'class': 'btn btn-lg btn-success btn-block'
+        }
+    )
+
+
+class UserDetailForm(FlaskForm):
+    name = StringField(
+        label='昵称',
+        validators=[
+            DataRequired('请输入昵称！')
+        ],
+        description='昵称',
+        render_kw={
+            'class': 'form-control',
+            'placeholder': '请输入昵称！',
+            'required': False
+        }
+    )
+    email = StringField(
+        label='邮箱',
+        validators=[
+            DataRequired('请输入邮箱！'),
+            Email('邮箱格式不正确！')
+        ],
+        description='邮箱',
+        render_kw={
+            'class': 'form-control',
+            'placeholder': '请输入邮箱！',
+            'required': False
+        }
+    )
+    phone = StringField(
+        label='手机号码',
+        validators=[
+            DataRequired('请输入手机号码！'),
+            Regexp('1[345789]\d{9}', message='手机号码格式不正确！')
+        ],
+        description='手机号码',
+        render_kw={
+            'class': 'form-control',
+            'placeholder': '请输入手机号码！',
+            'required': False
+        }
+    )
+    avatar = FileField(
+        label='头像',
+        validators=[
+            DataRequired('请上传头像！')
+        ],
+        description='头像',
+        render_kw={
+            'required': False
+        }
+    )
+    info = TextAreaField(
+        label='简介',
+        validators=[
+            DataRequired('请输入简介！'),
+        ],
+        description='简介',
+        render_kw={
+            'class': 'form-control',
+            'placeholder': '请输入简介！',
+            'rows': '10',
+            'required': False
+        }
+    )
+    submit = SubmitField(
+        label='保存修改',
+        render_kw={
+            'class': 'btn btn-success'
         }
     )
--- flask练习/movie_project/app/home/views.py
+++ flask练习/movie_project/app/home/views.py
@@ -20,10 +20,10 @@ from . import home
 from flask import render_template, redirect, url_for, flash, request, session, current_app
 
 
-# def change_filename(filename):
-#     fileinfo = os.path.splitext(filename)
-#     filename = datetime.now().strftime('%Y%m%d%H%M%S') + str(uuid.uuid4().hex) + fileinfo[-1]
-#     return filename
+def change_filename(filename):
+    fileinfo = os.path.splitext(filename)
+    filename = datetime.now().strftime('%Y%m%d%H%M%S') + str(uuid.uuid4().hex) + fileinfo[-1]
+    return filename
 
 
 # 登录装饰器
@@ -88,51 +88,51 @@ def register():
     return render_template('home/register.html', form=form)
 
 
-# @home.route('/user/', methods=['GET', 'POST'])
-# @user_login_req
-# def user():
-#     form = UserDetailForm()
-#     user = User.query.get(int(session['user_id']))
-#     form.avatar.validators = []
-#     if request.method == 'GET':
-#         form.name.data = user.name
-#         form.email.data = user.email
-#         form.phone.data = user.phone
-#         form.info.data = user.info
-#     if form.validate_on_submit():
-#         data = form.data
-#         print(form.name)
-#         print(form.avatar)
-#         print(form.avatar.data)
-#         print(type(form.avatar.data))
-#         if form.avatar.data:
-#             file_avatar = secure_filename(form.avatar.data.filename)
-#             if not os.path.exists(current_app.config['UP_DIR']):
-#                 os.makedirs(current_app.config['UP_DIR'])
-#             user.avatar = change_filename(file_avatar)
-#             form.avatar.data.save(current_app.config['UP_DIR'] + user.avatar)
-#
-#         name_count = User.query.filter_by(name=data['name']).count()
-#         if data['name'] != user.name and name_count == 1:
-#             flash('昵称已经存在！', 'err')
-#             return redirect(url_for('home.user'))
-#         email_count = User.query.filter_by(email=data['email']).count()
-#         if data['email'] != user.email and email_count == 1:
-#             flash('邮箱已经存在！', 'err')
-#             return redirect(url_for('home.user'))
-#         phone_count = User.query.filter_by(phone=data['phone']).count()
-#         if data['phone'] != user.phone and phone_count == 1:
-#             flash('手机号码已经存在！', 'err')
-#             return redirect(url_for('home.user'))
-#         user.name = data['name']
-#         user.email = data['email']
-#         user.phone = data['phone']
-#         user.info = data['info']
-#         db.session.add(user)
-#         db.session.commit()
-#         flash('修改成功！', 'ok')
-#         return redirect(url_for('home.user'))
-#     return render_template('home/user.html', form=form, user=user)
+@home.route('/user/', methods=['GET', 'POST'])
+@user_login_req
+def user():
+    form = UserDetailForm()
+    user = User.query.get(int(session['user_id']))
+    form.avatar.validators = []
+    if request.method == 'GET':
+        form.name.data = user.name
+        form.email.data = user.email
+        form.phone.data = user.phone
+        form.info.data = user.info
+    if form.validate_on_submit():
+        data = form.data
+        print(form.name)
+        print(form.avatar)
+        print(form.avatar.data)
+        print(type(form.avatar.data))
+        if form.avatar.data:
+            file_avatar = secure_filename(form.avatar.data.filename)
+            if not os.path.exists(current_app.config['UP_DIR']):
+                os.makedirs(current_app.config['UP_DIR'])
+            user.avatar = change_filename(file_avatar)
+            form.avatar.data.save(current_app.config['UP_DIR'] + user.avatar)
+
+        name_count = User.query.filter_by(name=data['name']).count()
+        if data['name'] != user.name and name_count == 1:
+            flash('昵称已经存在！', 'err')
+            return redirect(url_for('home.user'))
+        email_count = User.query.filter_by(email=data['email']).count()
+        if data['email'] != user.email and email_count == 1:
+            flash('邮箱已经存在！', 'err')
+            return redirect(url_for('home.user'))
+        phone_count = User.query.filter_by(phone=data['phone']).count()
+        if data['phone'] != user.phone and phone_count == 1:
+            flash('手机号码已经存在！', 'err')
+            return redirect(url_for('home.user'))
+        user.name = data['name']
+        user.email = data['email']
+        user.phone = data['phone']
+        user.info = data['info']
+        db.session.add(user)
+        db.session.commit()
+        flash('修改成功！', 'ok')
+        return redirect(url_for('home.user'))
+    return render_template('home/user.html', form=form, user=user)
 
 
 @home.route('/pwd/', methods=['GET', 'POST'])
